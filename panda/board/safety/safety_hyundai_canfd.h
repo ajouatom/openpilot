558652bde377c96753fd0e428cf5a9e6cfb72e4c
ajouatom <ajouatom@gmail.com>
2024-02-05 21:12:50 +0900

    ff

--- panda/board/safety/safety_hyundai_canfd.h
+++ panda/board/safety/safety_hyundai_canfd.h
@@ -191,6 +191,7 @@ static void hyundai_canfd_rx_hook(const CANPacket_t *to_push) {
       hyundai_common_cruise_buttons_check(cruise_button, main_button);
       int k;
       for (k = 0; k < 32; k++) { puth2(GET_BYTE(to_push, k)); if ((k % 2) == 0) print(" "); }
+      print("\n");
 
     }
 
