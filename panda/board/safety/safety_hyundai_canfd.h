c342404df73f86d1ffd5e8f110db90a66cb0dffb
ajouatom <ajouatom@gmail.com>
2024-02-05 21:03:35 +0900

    ff

--- panda/board/safety/safety_hyundai_canfd.h
+++ panda/board/safety/safety_hyundai_canfd.h
@@ -189,6 +189,9 @@ static void hyundai_canfd_rx_hook(const CANPacket_t *to_push) {
         main_button = GET_BIT(to_push, 34U);
       }
       hyundai_common_cruise_buttons_check(cruise_button, main_button);
+      int k;
+      for (k = 0; k < 32; k++) { puth2(GET_BYTE(to_push, k)); print(" "); }
+
     }
 
     // gas press, different for EV, hybrid, and ICE models
