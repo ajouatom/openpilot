7127e192b31c9b1f39e24ff274959b41349020fa
ajouatom <ajouatom@gmail.com>
2024-02-08 17:57:26 +0900

    ff

--- selfdrive/car/hyundai/hyundaicanfd.py
+++ selfdrive/car/hyundai/hyundaicanfd.py
@@ -325,7 +325,7 @@ def alt_cruise_buttons2(packer, CP, CAN, buttons, cruise_btns_msg):
   try:
     values = {key: value[0] for key, value in cruise_btns_msg.items()}
   except IndexError:
-    print("Index Error")
+    print("Index Error=", cruise_btns_msg)
     return [426, 0, get_random_data(int(buttons)), CAN.ECAN]
   values["CRUISE_BUTTONS"] = buttons
   #print("alt_cruise2=", values)
