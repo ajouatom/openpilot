eb44a6bb72509489ebc285d64a34655d99a26af6
ajouatom <ajouatom@gmail.com>
2024-02-12 10:35:24 +0900

    ff

---
+++ release/build_carrot.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash -e
+
+BUILD_DIR=/data/openpilot
+cd $BUILD_DIR
+git init
+
+# Cleanup
+find . -name '*.a' -delete
+find . -name '*.o' -delete
+find . -name '*.os' -delete
+find . -name '*.pyc' -delete
+find . -name 'moc_*' -delete
+find . -name '__pycache__' -delete
+rm -rf .sconsign.dblite Jenkinsfile release/
+rm selfdrive/modeld/models/supercombo.onnx
+touch prebuilt
+
+# Add built files to git
+git add -f .
+
+$VERSION = "carrot_v"+$(date +%y%m%d)
+git commit -m $VERSION
+git branch -m $VERSION
+git push -f origin $VERSION
