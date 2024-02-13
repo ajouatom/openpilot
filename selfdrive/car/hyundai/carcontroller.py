994bdc54b086c2d5c3d2b2b4c43c447ed1bc20bd
ajouatom <ajouatom@gmail.com>
2024-02-14 07:31:26 +0900

    ff

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -425,8 +425,7 @@ class CarController:
       self.last_button_frame = self.frame
 
     if (self.frame - self.last_button_frame) > self.button_wait:
-      #self.button_wait = randint(4,16)
-      self.button_wait = 12
+      self.button_wait = randint(8,12)
       self.last_button_frame = self.frame
 
       if not CC.enabled:
