cc4cefee42043bec99ec0e2c5708ad1c41c4a4fb
ajouatom <ajouatom@gmail.com>
2024-02-19 12:53:53 +0900

    ff

--- selfdrive/sentry.py
+++ selfdrive/sentry.py
@@ -31,7 +31,7 @@ def report_tombstone(fn: str, message: str, contents: str) -> None:
 def capture_exception(*args, **kwargs) -> None:
   cloudlog.error("crash", exc_info=kwargs.get('exc_info', 1))
 
-  Params().put_bool("CarrotException")
+  Params().put_bool("CarrotException", True)
 
   try:
     sentry_sdk.capture_exception(*args, **kwargs)
