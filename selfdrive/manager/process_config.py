48b214648dacb2a0c2db0ce92cf23866a83512cf
ajouatom <ajouatom@gmail.com>
2024-02-20 07:19:51 +0900

    ff

--- selfdrive/manager/process_config.py
+++ selfdrive/manager/process_config.py
@@ -53,7 +53,7 @@ def enable_dm(started, params, CP: car.CarParams) -> bool:
   return (started or params.get_bool("IsDriverViewEnabled")) and params.get_int("ShowDmInfo") > 0
 
 def enable_logging(started, params, CP: car.CarParams) -> bool:
-  True
+  return True
   #return params.get_bool("NoLogging"))
 
 EnableOSM = Params().get_int("EnableOSM")
