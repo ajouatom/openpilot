72e8058d71ccde5f2bfa0c3e9637cdf07030740a
ajouatom <ajouatom@gmail.com>
2024-02-23 17:11:11 +0900

    ff

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -441,7 +441,7 @@ class CarController:
 
     speed_diff = self.prev_clu_speed - current
     self.button_spamming_speed_diff += speed_diff
-    spamming_max = 8
+    spamming_max = 12
     #if CS.cruise_buttons[-1] != Buttons.NONE or (abs(self.button_spamming_count) > spamming_max and abs(speed_diff) < 1) or abs(self.button_spamming_speed_diff) > 5:
     if CS.cruise_buttons[-1] != Buttons.NONE or abs(self.button_spamming_speed_diff) > 5:
       self.last_button_frame = self.frame
