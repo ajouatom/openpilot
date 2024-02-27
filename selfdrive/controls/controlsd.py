72add8bcbfaf5cbfbda042bd77ddc62011e05955
ajouatom <ajouatom@gmail.com>
2024-02-27 18:27:35 +0900

    fix... pcmCruise.... min 30

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -912,7 +912,7 @@ class Controls:
     hudControl = CC.hudControl
     v_cruise_kph_long = self.sm['longitudinalPlan'].vCruiseTarget
     if self.CP.pcmCruise:
-      hudControl.setSpeed = setSpeed if self.v_cruise_helper.speedFromPCM != 2 else float(v_cruise_kph_long * CV.KPH_TO_MS) #float(self.v_cruise_helper.v_cruise_cluster_kph * CV.KPH_TO_MS)
+      hudControl.setSpeed = max(30, setSpeed if self.v_cruise_helper.speedFromPCM != 2 else float(v_cruise_kph_long * CV.KPH_TO_MS)) #float(self.v_cruise_helper.v_cruise_cluster_kph * CV.KPH_TO_MS)
     else:
       hudControl.setSpeed = setSpeed if self.v_cruise_helper.xState == 3 else float(v_cruise_kph_long * CV.KPH_TO_MS) #float(self.v_cruise_helper.v_cruise_cluster_kph * CV.KPH_TO_MS)
     #print("setSpeed={:.1f}".format(hudControl.setSpeed * 3.6))
