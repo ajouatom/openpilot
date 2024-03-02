46b8f4a1a0c62ed888b5f6632d1cc76422efa295
ajouatom <ajouatom@gmail.com>
2024-03-02 18:31:39 +0900

    test

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1323,11 +1323,13 @@ void DrawApilot::drawSpeed(const UIState* s, int x, int y) {
         if (isEnabled() && isLongActive() && applyMaxSpeed > 0) {
             NVGcolor textColor = COLOR_GREEN;
             str[0] = 0;
+            char str_source[128] = "";
             static float dispApplyMaxSpeed = 0.0;
                 if (longVCruiseTarget < cruiseMaxSpeed - 0.5) {
                     //dispApplyMaxSpeed = dispApplyMaxSpeed * 0.95 + longVCruiseTarget * (s->scene.is_metric ? 1.0 : KM_TO_MILE) * 0.05;
                     dispApplyMaxSpeed = longVCruiseTarget * (s->scene.is_metric ? 1.0 : KM_TO_MILE);
-                    sprintf(str, "%d %s", (int)(dispApplyMaxSpeed + 0.5), longVCruiseTargetSource.toStdString().c_str());
+                    sprintf(str, "%d", (int)(dispApplyMaxSpeed + 0.5));
+                    strcpy(str_source, longVCruiseTargetSource.toStdString().c_str());
                     textColor = COLOR_OCHRE;
                 }
                 else if (applyMaxSpeed != cruiseMaxSpeed) {
@@ -1336,9 +1338,10 @@ void DrawApilot::drawSpeed(const UIState* s, int x, int y) {
                 }
                 else dispApplyMaxSpeed = cruiseMaxSpeed;
             if (strlen(str)) {
-                nvgTextAlign(s->vg, NVG_ALIGN_LEFT | NVG_ALIGN_BOTTOM);
-                ui_draw_text(s, bx + 220, by - 50, str, 50, textColor, BOLD, 1.0, 5.0, COLOR_BLACK, COLOR_BLACK);
-                nvgTextAlign(s->vg, NVG_ALIGN_CENTER | NVG_ALIGN_BOTTOM);
+                ui_draw_text(s, bx + 250, by - 50, str, 50, textColor, BOLD, 1.0, 5.0, COLOR_BLACK, COLOR_BLACK);
+                if (strlen(str_source)) {
+                    ui_draw_text(s, bx + 250, by - 90, str_source, 30, textColor, BOLD, 1.0, 5.0, COLOR_BLACK, COLOR_BLACK);
+                }
             }
         }
 
