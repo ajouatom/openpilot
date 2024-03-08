f9144f53a5f311df360c20c13e1300d48953529e
ajouatom <ajouatom@gmail.com>
2024-03-08 23:02:05 +0900

    ff

--- selfdrive/car/car_helpers.py
+++ selfdrive/car/car_helpers.py
@@ -209,7 +209,7 @@ def get_car(logcan, sendcan, experimental_long_allowed, num_pandas=1):
       from openpilot.selfdrive.car.hyundai.values import CAR as HYUNDAI
       from openpilot.selfdrive.car.gm.values import CAR as GM
       from openpilot.selfdrive.car.toyota.values import CAR as TOYOTA
-      for car in HYUNDAI:
+      for car in HYUNDAI, GM, TOYOTA:
         if car.config.platform_str == name:
           return car
       return None
