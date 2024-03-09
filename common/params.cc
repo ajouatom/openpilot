c7c5d6703d29c10f40a2d7cdafd4a76f142590a4
ajouatom <ajouatom@gmail.com>
2024-03-09 16:37:08 +0900

    ff

--- common/params.cc
+++ common/params.cc
@@ -323,6 +323,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     { "NoLogging", PERSISTENT },
     { "CarrotException", CLEAR_ON_MANAGER_START },
     { "CarrotRouteActive", CLEAR_ON_MANAGER_START },
+    { "CarName", CLEAR_ON_MANAGER_START },
 
     { "AlwaysOnLateralEnabled", PERSISTENT},
     { "GMapKey", PERSISTENT},
