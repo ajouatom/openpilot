bb48bab59fd0fc77f786942d9237a2da5e89e6f9
ajouatom <ajouatom@gmail.com>
2024-03-10 19:44:27 +0900

    ff

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -130,7 +130,7 @@ class CarInterface(CarInterfaceBase):
 
     if Params().get_bool("SccConnectedBus2"):
       ret.flags |= HyundaiFlags.SCC_BUS2.value
-      experimental_long = True
+      #experimental_long = True
     print("***************************************************************************")
     print("sccBus = ", 2 if ret.flags & HyundaiFlags.SCC_BUS2.value else 0)
 
