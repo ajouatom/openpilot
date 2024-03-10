873a743076ca91d12119f39af44ad5fbfc297251
ajouatom <ajouatom@gmail.com>
2024-03-10 17:11:19 +0900

    ff

--- panda/board/safety/safety_hyundai_canfd.h
+++ panda/board/safety/safety_hyundai_canfd.h
@@ -343,7 +343,7 @@ static int hyundai_canfd_fwd_hook(int bus_num, int addr) {
 #if 1
       uint32_t now = microsecond_timer_get();
 
-      bus_fwd = 0
+      bus_fwd = 0;
       for (int i = 0; canfd_tx_addr[i] > 0; i++) {
           if (addr == canfd_tx_addr[i] && (now - canfd_tx_time[i]) < 200000) {
               bus_fwd = -1;
