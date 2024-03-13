1870e7916cff8860f1de57e4110ea27fcee6b89b
ajouatom <ajouatom@gmail.com>
2024-03-13 10:48:54 +0900

    ㄹㄹ

--- selfdrive/controls/radard.py
+++ selfdrive/controls/radard.py
@@ -362,12 +362,12 @@ class VisionTrack:
     self.P_v = 0.5 #1.0
     self.v_ego = 0.0
 
-  # 프로세스노이즈Q: 값을 올리면 측정값에 대해 민감하게 반응함
+  # 프로세스노이즈Q: 값을 올리면 측정값에 대해 민감하게 반응함, 응답성이 빨라짐.
   # 측정노이즈R: 값을 낮추면 측정값에 대해 더 신뢰하게 됨.
   def v_rel_k(self, vel):
     vRelK = self.vRelK
-    Q = 0.1 #0.01 #0.1   
-    R = 3.0 #5.0
+    Q = 0.2 #0.01 #0.1   
+    R = 7.0 #5.0
     P_predict = self.P_v + Q
     z = vel / self.radar_ts
     K = P_predict / (P_predict + R)
@@ -411,7 +411,7 @@ class VisionTrack:
         #vLead = self.vLeadFilter.process(float(v_ego + lead_v_rel_pred))
         vLead = self.vLeadFilter.process(lead_msg.v[0])
         self.a_lead_k(vLead - self.vLead)
-        if abs(dRel - self.dRel) > 1.0:
+        if abs(dRel - self.dRel) > 0.8:
           self.vRelK = 0.0          
           self.vLeadK = vLead
           self.v_rel_k(dRel - self.dRel)
