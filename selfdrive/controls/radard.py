35ae8289b8fa495169f4d677370a34eed4f1195c
ajouatom <ajouatom@gmail.com>
2024-03-13 15:29:42 +0900

    ff

--- selfdrive/controls/radard.py
+++ selfdrive/controls/radard.py
@@ -536,7 +536,8 @@ class RadarD:
       model_v_ego = self.v_ego
     leads_v3 = sm['modelV2'].leadsV3
     if len(leads_v3) > 1:
-      self.vision_track.update(leads_v3[0], model_v_ego, self.v_ego)
+      if sm['modelV2'].updated():
+        self.vision_track.update(leads_v3[0], model_v_ego, self.v_ego)
       if self.mixRadarInfo in [1,2]: ## leadOne: radar or vision, leadTwo: vision 
         self.radar_state.leadOne = self.get_lead(self.tracks, leads_v3[0], model_v_ego, low_speed_override=False)
         self.radar_state.leadTwo = self.get_lead(self.tracks_empty, leads_v3[0], model_v_ego, low_speed_override=False)
