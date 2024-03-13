874c75a57d1ef8317a2da89e9268c8d696180dfe
ajouatom <ajouatom@gmail.com>
2024-03-13 19:27:20 +0900

    ff

--- selfdrive/car/hyundai/hyundaicanfd.py
+++ selfdrive/car/hyundai/hyundaicanfd.py
@@ -37,7 +37,7 @@ class CanBus(CanBusBase):
 
 def carrot_canfd353(packer, CAN, lat_active, canfd353_info):
   values = canfd353_info
-  values['BIT27'] = 1 if lat_active else 0
+  values['BIT27'] = 1# if lat_active else 0
   return packer.make_can_msg("CANFD353", CAN.ECAN, values)
 
 def create_steering_messages_scc2(packer, CP, CAN, enabled, lat_active, apply_steer, lfa_info):
