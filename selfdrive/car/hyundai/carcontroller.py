e30a51303bb6a0045763b9c7574a9e200246dbcc
ajouatom <ajouatom@gmail.com>
2024-03-15 21:29:42 +0900

    ff

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -234,8 +234,8 @@ class CarController(CarControllerBase):
       #else:
       #  can_sends.extend(hyundaicanfd.create_steering_messages(self.packer, self.CP, self.CAN, CC.enabled, apply_steer_req, apply_steer))
 
-      if self.CP.flags & HyundaiFlags.SCC_BUS2.value and self.CP.flags & HyundaiFlags.CANFD_HDA2:
-        can_sends.append(hyundaicanfd.carrot_canfd353(self.packer, self.CAN, CC.latActive, CS.canfd353_info))
+      #if self.CP.flags & HyundaiFlags.SCC_BUS2.value and self.CP.flags & HyundaiFlags.CANFD_HDA2:
+      #  can_sends.append(hyundaicanfd.carrot_canfd353(self.packer, self.CAN, CC.latActive, CS.canfd353_info))
 
       # prevent LFA from activating on HDA2 by sending "no lane lines detected" to ADAS ECU
       if self.frame % 5 == 0 and hda2 and not (self.CP.flags & HyundaiFlags.SCC_BUS2.value): # SCC_BUS2의 경우 ACAN이 bus1에 있어서 보낼수가 없음..
