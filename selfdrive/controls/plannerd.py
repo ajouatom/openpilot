3673d9cbf69a8708cc4055e3dde6f5c96c953bf1
ajouatom <ajouatom@gmail.com>
2024-03-17 21:04:41 +0900

    ff

--- selfdrive/controls/plannerd.py
+++ selfdrive/controls/plannerd.py
@@ -28,10 +28,10 @@ def publish_ui_plan(sm, pm, lateral_planner, longitudinal_planner):
   else:
     plan_odo = cumtrapz(longitudinal_planner.v_desired_trajectory_full, ModelConstants.T_IDXS)
     model_odo = cumtrapz(lateral_planner.v_plan, ModelConstants.T_IDXS)
-    uiPlan.position.x = np.interp(plan_odo, model_odo, lateral_planner.x_sol[:,0]).tolist()
-    uiPlan.position.y = np.interp(plan_odo, model_odo, lateral_planner.x_sol[:,1]).tolist()
-    #uiPlan.position.x = np.interp(plan_odo, model_odo, lateral_planner.path_xyz[:,0]).tolist()
-    #uiPlan.position.y = np.interp(plan_odo, model_odo, lateral_planner.path_xyz[:,1]).tolist()
+    #uiPlan.position.x = np.interp(plan_odo, model_odo, lateral_planner.x_sol[:,0]).tolist()
+    #uiPlan.position.y = np.interp(plan_odo, model_odo, lateral_planner.x_sol[:,1]).tolist()
+    uiPlan.position.x = np.interp(plan_odo, model_odo, lateral_planner.path_xyz[:,0]).tolist()
+    uiPlan.position.y = np.interp(plan_odo, model_odo, lateral_planner.path_xyz[:,1]).tolist()
     uiPlan.position.z = np.interp(plan_odo, model_odo, lateral_planner.path_xyz[:,2]).tolist()
   
   uiPlan.accel = longitudinal_planner.a_desired_trajectory_full.tolist()
