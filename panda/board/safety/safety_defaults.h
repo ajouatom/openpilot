1b2d8c64667f756606f3b83d70c415f6bdd2a41f
ajouatom <ajouatom@gmail.com>
2024-03-24 23:25:38 +0900

    ff

--- panda/board/safety/safety_defaults.h
+++ panda/board/safety/safety_defaults.h
@@ -54,7 +54,7 @@ static int alloutput_fwd_hook(int bus_num, int addr) {
   if (alloutput_passthrough) {
     if (bus_num == 0) {
       bus_fwd = 2;
-      if (addr == 272 || addr == 80 || addr == 81) { // || addr == 866 || addr == 676) {
+      if (addr == 272 || addr == 80) { // || addr == 81) { // || addr == 866 || addr == 676) {
           last_ts_lkas_msg_acan = now;
           lkas_msg_acan_active = true;
           print("blocking\n");
@@ -62,7 +62,7 @@ static int alloutput_fwd_hook(int bus_num, int addr) {
     }
     if (bus_num == 2) {
       bus_fwd = 0;
-      if (addr == 272 || addr == 80 || addr == 81) { // || addr == 866 || addr == 676) {
+      if (addr == 272 || addr == 80) { // || addr == 81) { // || addr == 866 || addr == 676) {
           if (now - last_ts_lkas_msg_acan < 200000) {
               bus_fwd = -1;
           }
