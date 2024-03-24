f27638bb11e74bd9ee1d7b2a9f8fe54229f923fc
ajouatom <ajouatom@gmail.com>
2024-03-24 21:10:05 +0900

    ff

--- panda/board/drivers/fdcan.h
+++ panda/board/drivers/fdcan.h
@@ -147,7 +147,7 @@ void process_can(uint8_t can_number) {
 void carrot_acan_function(int bus_num, CANPacket_t *to_send) {
     if (!lkas_msg_acan_active) return;
     int addr = GET_ADDR(to_send);
-    if (bus_num == 2) {
+    if (bus_num == 0) {
         if (addr == 866 || addr == 676) {       // 0x362, 0x2a4, 56,57,58,59,60,61,62,63 => make zero
             to_send->data[7] = 0x00;
         }
