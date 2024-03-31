ebb634d7ac7c9061a8f42b2fe63b6cb2e7e742ac
ajouatom <ajouatom@gmail.com>
2024-03-31 15:18:19 +0900

    canfd scc_control

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -257,7 +257,7 @@ class CarController(CarControllerBase):
           else:
             can_sends.extend(hyundaicanfd.create_fca_warning_light(self.packer, self.CAN, self.frame))
         if self.frame % 2 == 0:
-          if False: #self.CP.extFlags & HyundaiExtFlags.SCC_BUS2.value:
+          if self.CP.extFlags & HyundaiExtFlags.SCC_BUS2.value:
             can_sends.append(hyundaicanfd.create_acc_control_scc2(self.packer, self.CAN, CC.enabled, self.accel_last, accel, stopping, CC.cruiseControl.override,
                                                              set_speed_in_units, hud_control, jerk_u, jerk_l, CS.cruise_info))
           else:
