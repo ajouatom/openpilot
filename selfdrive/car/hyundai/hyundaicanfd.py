df6d85fcb6d9464ca0097998085f5325c9b68d1c
ajouatom <ajouatom@gmail.com>
2024-04-02 17:47:16 +0900

    revert

--- selfdrive/car/hyundai/hyundaicanfd.py
+++ selfdrive/car/hyundai/hyundaicanfd.py
@@ -246,10 +246,10 @@ def create_adrv_messages(CP, packer, CAN, frame):
 
   values = {
   }
-  if True: #not (CP.extFlags & HyundaiExtFlags.SCC_BUS2.value) or CP.extFlags & HyundaiExtFlags.ACAN_PANDA.value:
+  if not (CP.extFlags & HyundaiExtFlags.SCC_BUS2.value) or CP.extFlags & HyundaiExtFlags.ACAN_PANDA.value:
     ret.append(packer.make_can_msg("ADRV_0x51", CAN.ACAN, values))
 
-  if True: #not (CP.extFlags & HyundaiExtFlags.SCC_BUS2.value):
+  if not (CP.extFlags & HyundaiExtFlags.SCC_BUS2.value):
     ret.extend(create_fca_warning_light(packer, CAN, frame))
     if frame % 5 == 0:
       values = {
