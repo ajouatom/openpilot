bf67cae92dd0418af530d2649472d40d81dbc978
ajouatom <ajouatom@gmail.com>
2024-04-10 18:39:23 +0900

    ff

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1685,12 +1685,12 @@ void DrawApilot::drawPathEnd(const UIState* s, int x, int y, int path_x, int pat
             //ui_draw_text(s, x, disp_y, str, disp_size, COLOR_WHITE, BOLD);
             int wStr = 0, w=80;
             sprintf(str, "%.1f", getRadarDist());
-            wStr = 35 * (strlen(str) + 0);
+            wStr = 32 * (strlen(str) + 0);
             ui_fill_rect(s->vg, { (int)(x - w - wStr / 2), (int)(disp_y - 35), wStr, 42 }, COLOR_RED, 15);
             ui_draw_text(s, x - w, disp_y, str, 40, COLOR_WHITE, BOLD);
 
             sprintf(str, "%.1f", getVisionDist());
-            wStr = 35 * (strlen(str) + 0);
+            wStr = 32 * (strlen(str) + 0);
             ui_fill_rect(s->vg, { (int)(x + w - wStr / 2), (int)(disp_y - 35), wStr, 42 }, COLOR_BLUE, 15);
             ui_draw_text(s, x + w, disp_y, str, 40, COLOR_WHITE, BOLD);
 
