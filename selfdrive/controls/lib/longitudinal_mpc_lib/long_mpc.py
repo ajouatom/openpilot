74439c8e6a6675001d7570cfca1f20eebd85ab75
carrot <43668841+ajouatom@users.noreply.github.com>
2024-04-18 22:37:14 +0900

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -698,7 +698,7 @@ class LongitudinalMpc:
         else:
           self.comfort_brake = COMFORT_BRAKE * 0.9
           #self.comfort_brake = COMFORT_BRAKE
-          self.trafficStopAdjustRatio = 0.85
+          self.trafficStopAdjustRatio = 0.8
           stop_dist = self.xStop * interp(self.xStop, [0, 100], [1.0, self.trafficStopAdjustRatio])  ##남은거리에 따라 정지거리 비율조정
           if stop_dist > 5.0:
             self.stopDist = stop_dist
