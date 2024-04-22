93d7c21910468ad0472ee9d8f0cc227b59a14b55
ajouatom <ajouatom@gmail.com>
2024-04-22 19:25:14 +0900

    ff

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -157,8 +157,8 @@ def get_nn_model_path(car, eps_firmware) -> Tuple[Union[str, None, float]]:
           model_path = os.path.join(TORQUE_NN_MODEL_PATH, f)
     return model_path, max_similarity
 
-  car = car.replace('_', ' ')
-  car = car.replace('HEV', 'HYBRID')
+  car1 = car.replace('_', ' ')
+  car = car1.replace('HEV', 'HYBRID')
   print("########get_nn_model_path :", car, eps_firmware)
   if len(eps_firmware) > 3:
     eps_firmware = eps_firmware.replace("\\", "")
