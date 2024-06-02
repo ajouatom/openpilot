a0e7f92e9e4b78845db950d3dc38cdb90e07e73a
ajouatom <ajouatom@gmail.com>
2024-06-02 20:30:12 +0900

    ff

--- selfdrive/carrot/carrot_man.py
+++ selfdrive/carrot/carrot_man.py
@@ -145,7 +145,6 @@ class CarrotMan:
 
   def save_toggle_values(self):
     toggle_values = fleet.get_all_toggle_values()
-    modified_toggle_values = {key: f"{value}cr" for key, value in toggle_values.items()}
     file_path = os.path.join('/data', 'toggle_values.json')
     with open(file_path, 'w') as file:
       json.dump(modified_toggle_values, file, indent=2) 
