c424a981798ab7a664325b94a5993c6019033c3a
ajouatom <ajouatom@gmail.com>
2024-06-02 20:29:07 +0900

    ff

--- selfdrive/carrot/carrot_man.py
+++ selfdrive/carrot/carrot_man.py
@@ -148,7 +148,7 @@ class CarrotMan:
     modified_toggle_values = {key: f"{value}cr" for key, value in toggle_values.items()}
     file_path = os.path.join('/data', 'toggle_values.json')
     with open(file_path, 'w') as file:
-      json.dump(toggle_values, file)    
+      json.dump(modified_toggle_values, file, indent=2) 
 
   def carrot_cmd_zmq(self):
 
