5761ab9c530fa213ce48b1d26dcd2a3726563364
yoonch9009 <yoonch9009@naver.com>
2024-06-16 20:22:06 +0900

    values.py flags 삭제

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -351,7 +351,6 @@ class CAR(Platforms):
   HYUNDAI_CASPER = HyundaiPlatformConfig(
   [HyundaiCarDocs("Hyundai Casper 2022", "All", car_parts=CarParts.common([CarHarness.hyundai_a]))],
   CarSpecs(mass=1060, wheelbase=2.4, steerRatio=14.3), # https://namu.wiki/w/%ED%98%84%EB%8C%80%20%EC%BA%90%EC%8A%A4%ED%8D%BC#s-3
-  flags=HyundaiFlags.CHECKSUM_CRC8,
   )
 
   # Kia
