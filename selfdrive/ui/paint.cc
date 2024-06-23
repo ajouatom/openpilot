17aead0ce27ae59c3b984fbe9d68e00b5f90390b
ajouatom <ajouatom@gmail.com>
2024-06-23 19:47:37 +0900

    ff

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1435,7 +1435,7 @@ void DrawApilot::drawSpeed(const UIState* s, int x, int y) {
             if (s->xDistToTurn >= 200) scale = 0.5;
             else if (s->xDistToTurn <= 0) scale = 1.0;
             else scale = 1.0 - (0.5 * (s->xDistToTurn / 200.0));
-            printf("%d, %.2f\n", s->xDistToTurn, scale);
+            printf("%d, %.2f\n", (int) s->xDistToTurn, scale);
             int size_x = 348 * scale;
             int size_y = 440 * scale;
             int img_x = (int)s->navi_turn_point[0].x() - size_x;
