14511726ea4fd0ab1c98200249ddd9479f2074de
ajouatom <ajouatom@gmail.com>
2024-06-23 19:44:02 +0900

    ff

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -1432,9 +1432,9 @@ void DrawApilot::drawSpeed(const UIState* s, int x, int y) {
             //ui_draw_bsd(s, s->xTurnInfo_vertices, &color, false);
             //ui_draw_line(s, s->xTurnInfo_vertices, &color, nullptr);
             float scale = 1.0;
-            if (s->xDistToTurn >= 100) scale = 0.5;
+            if (s->xDistToTurn >= 200) scale = 0.5;
             else if (s->xDistToTurn < 10) scale = 1.0;
-            else scale = 1.0 - (0.5 * (s->xDistToTurn / 100.0));
+            else scale = 1.0 - (0.5 * (s->xDistToTurn / 200.0));
             int size_x = 348 * scale;
             int size_y = 440 * scale;
             int img_x = (int)s->navi_turn_point[0].x() - size_x;
