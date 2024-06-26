a1496082373031fd30e4028a7d02c67d8dd773ce
ajouatom <ajouatom@gmail.com>
2024-06-26 09:32:59 +0900

    ff

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -545,6 +545,7 @@ void update_model(UIState *s,
   kkk--;
   if (kkk < 0) kkk = 300;
   s->left_dist = kkk;
+  s->xSignType = 124;
   if (s->left_dist > 0) {
       int idx = get_path_length_idx(road_edges[0], s->left_dist);
       calib_frame_to_full_frame(s, road_edges[1].getX()[idx], road_edges[1].getY()[idx], road_edges[1].getZ()[idx], &s->left_dist_point);
