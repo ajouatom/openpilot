4decf898c35208e29fb7ffd3e29e36ba9de9e2b7
Bogle <yoonch9009@naver.com>
2024-06-29 21:00:05 +0900

    Update values.py

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -348,10 +348,6 @@ class CAR(Platforms):
     CarSpecs(mass=1690, wheelbase=3.055, steerRatio=17),  # mass: from https://www.hyundai-motor.com.tw/clicktobuy/custin#spec_0, steerRatio: from learner
     flags=HyundaiFlags.CHECKSUM_CRC8,
   )
-  HYUNDAI_CASPER = HyundaiPlatformConfig(
-  [HyundaiCarDocs("Hyundai Casper 2022", "All", car_parts=CarParts.common([CarHarness.hyundai_a]))],
-  CarSpecs(mass=1060, wheelbase=2.4, steerRatio=14.3), # https://namu.wiki/w/%ED%98%84%EB%8C%80%20%EC%BA%90%EC%8A%A4%ED%8D%BC#s-3
-  )
 
   # Kia
   KIA_FORTE = HyundaiPlatformConfig(
@@ -630,6 +626,10 @@ class CAR(Platforms):
     [HyundaiCarDocs("Kia Soul EV 2019", car_parts=CarParts.common([CarHarness.hyundai_c]))],
     CarSpecs(mass=1695, wheelbase=2.6, steerRatio=13.75),
     flags=HyundaiFlags.CHECKSUM_CRC8 | HyundaiFlags.EV,
+  )
+  HYUNDAI_CASPER = HyundaiPlatformConfig(
+  [HyundaiCarDocs("Hyundai Casper 2022-2024", "All", car_parts=CarParts.common([CarHarness.hyundai_a]))],
+  CarSpecs(mass=1060, wheelbase=2.4, steerRatio=14.3), # https://namu.wiki/w/%ED%98%84%EB%8C%80%20%EC%BA%90%EC%8A%A4%ED%8D%BC#s-3
   )
 
 
