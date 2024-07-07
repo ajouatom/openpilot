91e683cdd7bdf686b6c58eb6d785ac59e4a703c1
ajouatom <ajouatom@gmail.com>
2024-07-07 12:34:30 +0900

    fix..

--- selfdrive/controls/radard.py
+++ selfdrive/controls/radard.py
@@ -506,8 +506,8 @@ class RadarD:
 
     # *** publish radarState ***
     self.radar_state_valid = sm.all_checks() and len(radar_errors) == 0
-    if not self.radar_state_valid:
-      print("radard_valid", sm.valid)
+    #if not self.radar_state_valid:
+    #  print("radard_valid", sm.valid)
     self.radar_state = log.RadarState.new_message()
     model_updated = False if self.radar_state.mdMonoTime == sm.logMonoTime['modelV2'] else True
     self.radar_state.mdMonoTime = sm.logMonoTime['modelV2']
