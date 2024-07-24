e6b79a665b4d6795c522c3390075923e786459ed
kans <kandrea@nate.com>
2024-07-24 17:24:57 +0900

    remove ModelSelect (#83)

--- selfdrive/ui/qt/home.h
+++ selfdrive/ui/qt/home.h
@@ -39,9 +39,6 @@ private:
   OffroadAlert* alerts_widget;
   QPushButton* alert_notif;
   QPushButton* update_notif;
-
-  // Model Select
-  QString modelName;
 };
 
 class HomeWindow : public QWidget {
