abe95b1b75729474b59095d817f29917471e512a
ajouatom <ajouatom@gmail.com>
2024-09-25 10:44:36 +0900

    ff

--- selfdrive/controls/neokii/navi_controller.py
+++ selfdrive/controls/neokii/navi_controller.py
@@ -166,6 +166,7 @@ class NaviServer:
       if ret:
         data, self.remote_addr = sock.recvfrom(2048)
         json_obj = json.loads(data.decode())
+        print('udp_recv=', json_obj)
 
         if 'cmd' in json_obj:
           try:
@@ -386,6 +387,7 @@ def main():
 
     except Exception as e:
       server.last_exception = e
+      print('!!!!!!!!!!!!!!!!!!! navi_controller: Exception:', e)
 
 class SpeedLimiter:
   __instance = None
