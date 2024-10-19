6c664ece5078074bf2a2eb695ea19c1630c5ce38
ajouatom <ajouatom@gmail.com>
2024-10-19 12:46:35 +0900

    fix..

--- selfdrive/car/cruise.py
+++ selfdrive/car/cruise.py
@@ -586,7 +586,10 @@ class VCruiseCarrot:
       self._cruise_ready = False
       self._brake_pressed_count = max(1, self._brake_pressed_count + 1)
       self._soft_hold_count = self._soft_hold_count + 1 if CS.vEgo < 0.1 and CS.gearShifter == GearShifter.drive else 0
-      self._soft_hold_active = 1 if self._soft_hold_count > 60 and not self.CP.pcmCruise else 0
+      if self.autoCruiseControl == 0 or self.CP.pcmCruise:
+        self._soft_hold_active = 0
+      else:
+        self._soft_hold_active = 1 if self._soft_hold_count > 60 else 0
     else:
       self._soft_hold_count = 0
       self._brake_pressed_count = min(-1, self._brake_pressed_count - 1)
