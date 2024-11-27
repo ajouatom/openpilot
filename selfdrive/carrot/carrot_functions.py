93834c3c6461a5660cadbe9f032252a4a07bcc29
ajouatom <ajouatom@gmail.com>
2024-11-27 17:27:50 +0900

    fix.. safe,eco mode factor...

--- selfdrive/carrot/carrot_functions.py
+++ selfdrive/carrot/carrot_functions.py
@@ -154,7 +154,7 @@ class CarrotPlanner:
 
   def get_carrot_accel(self, v_ego):
     cruiseMaxVals = [self.cruiseMaxVals1, self.cruiseMaxVals2, self.cruiseMaxVals3, self.cruiseMaxVals4, self.cruiseMaxVals5, self.cruiseMaxVals6]
-    factor = self.myHighModeFactor if self.myDrivingMode == 4 else 1.0
+    factor = self.myHighModeFactor if self.myDrivingMode == 4 else self.mySafeFactor
     return interp(v_ego, A_CRUISE_MAX_BP_CARROT, cruiseMaxVals) * factor
 
   def get_T_FOLLOW(self, personality=log.LongitudinalPersonality.standard):
