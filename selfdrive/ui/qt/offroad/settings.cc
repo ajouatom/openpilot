47acb2a173ca1db584d16be92af89a466eccb129
crwusiz <crwusiz@gmail.com>
2024-12-13 21:55:50 +0900

    fix
    change color ( reboot / select car )

--- selfdrive/ui/qt/offroad/settings.cc
+++ selfdrive/ui/qt/offroad/settings.cc
@@ -212,8 +212,8 @@ DevicePanel::DevicePanel(SettingsWindow *parent) : ListWidget(parent) {
   }
 
   setStyleSheet(R"(
-    #reboot_btn { height: 120px; border-radius: 15px; background-color: #393939; }
-    #reboot_btn:pressed { background-color: #4a4a4a; }
+    #reboot_btn { height: 120px; border-radius: 15px; background-color: #2CE22C; }
+    #reboot_btn:pressed { background-color: #24FF24; }
     #poweroff_btn { height: 120px; border-radius: 15px; background-color: #E22C2C; }
     #poweroff_btn:pressed { background-color: #FF2424; }
   )");
@@ -633,17 +633,18 @@ CarrotPanel::CarrotPanel(QWidget* parent) : QWidget(parent) {
     QPushButton* selectCarBtn = new QPushButton(selected.length() > 1 ? selected : tr("SELECT YOUR CAR"));
     selectCarBtn->setObjectName("selectCarBtn");
     selectCarBtn->setStyleSheet(R"(
-        QPushButton {
-            margin-top: 20px;  /* 위쪽 여백 */
-            margin-bottom: 20px;  /* 아래쪽 여백 */
-            padding: 10px;  /* 버튼 안쪽 패딩 */
-            height: 120px;  /* 버튼 높이 */
-            border-radius: 15px;  /* 버튼 모서리 둥글게 */
-            background-color: #393939;  /* 버튼 배경색 */
-        }
-        QPushButton:pressed {
-            background-color: #4a4a4a;  /* 버튼 클릭 시 배경색 */
-        }
+      QPushButton {
+        margin-top: 20px;  /* 위쪽 여백 */
+        margin-bottom: 20px;  /* 아래쪽 여백 */
+        padding: 10px;  /* 버튼 안쪽 패딩 */
+        height: 120px;  /* 버튼 높이 */
+        border-radius: 15px;  /* 버튼 모서리 둥글게 */
+        color: #FFFFFF;  /* 버튼 색상 */
+        background-color: #2C2CE2;  /* 버튼 배경색 */
+      }
+      QPushButton:pressed {
+        background-color: #2424FF;  /* 버튼 클릭 시 배경색 */
+      }
     )");
     //selectCarBtn->setFixedSize(350, 100);
     connect(selectCarBtn, &QPushButton::clicked, [=]() {
