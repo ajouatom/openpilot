ad09654f40401d6f13f90f900fb89568be1b39a6
ajouatom <ajouatom@gmail.com>
2025-02-15 21:12:39 +0900

    display route speed..

--- selfdrive/carrot/carrot_man.py
+++ selfdrive/carrot/carrot_man.py
@@ -1458,7 +1458,7 @@ class CarrotServ:
         source = "gas"
         desired_speed = self.gas_override_speed
 
-      self.debugText = ""#f"desired={desired_speed:.1f},{source},g={self.gas_override_speed:.0f}"
+      self.debugText = f"route={route_speed:.1f}"#f"desired={desired_speed:.1f},{source},g={self.gas_override_speed:.0f}"
 
     left_spd_sec = 100
     left_tbt_sec = 100
