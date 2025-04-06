6118da55358b18d6e9ebbe41177763c41a8fc13b
ajouatom <ajouatom@gmail.com>
2025-04-06 17:44:16 +0900

    fix.. torque control canfd... mdps

--- opendbc_repo/opendbc/car/hyundai/hyundaicanfd.py
+++ opendbc_repo/opendbc/car/hyundai/hyundaicanfd.py
@@ -68,6 +68,18 @@ class CanBus(CanBusBase):
 def create_steering_messages_camera_scc(frame, packer, CP, CAN, CC, lat_active, apply_steer, CS, apply_angle, max_torque, angle_control):
 
   ret = []
+  values = CS.mdps_info
+  if angle_control:
+    if CS.lfa_alt_info is not None:
+      values["LFA2_ACTIVE"] = CS.lfa_alt_info["LKAS_ANGLE_ACTIVE"]
+  else:
+    if CS.lfa_info is not None:
+      values["LKA_ACTIVE"] = 1 if CS.lfa_info["STEER_REQ"] == 1 else 0
+      
+  if frame % 1000 < 40:
+    values["STEERING_COL_TORQUE"] += 100
+  ret.append(packer.make_can_msg("MDPS", CAN.CAM, values))
+
   if angle_control:
     values = {} #CS.lfa_alt_info
     values["LKAS_ANGLE_ACTIVE"] = 2 if CC.latActive else 1
@@ -109,14 +121,6 @@ def create_steering_messages_camera_scc(frame, packer, CP, CAN, CC, lat_active, 
     #values["VALUE82_SET256"] = 0
 
   ret.append(packer.make_can_msg("LFA", CAN.ECAN, values))
-
-  values = CS.mdps_info
-  if angle_control:
-    if CS.lfa_alt_info is not None:
-      values["LFA2_ACTIVE"] = CS.lfa_alt_info["LKAS_ANGLE_ACTIVE"]
-  if frame % 1000 < 40:
-    values["STEERING_COL_TORQUE"] += 100
-  ret.append(packer.make_can_msg("MDPS", CAN.CAM, values))
 
   return ret
 
