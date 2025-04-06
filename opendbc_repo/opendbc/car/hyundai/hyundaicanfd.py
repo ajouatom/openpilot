bc7c2c085d6dd0e4f6f26910d0f4e7b18969edcf
ajouatom <ajouatom@gmail.com>
2025-04-06 19:42:07 +0900

    ff

--- opendbc_repo/opendbc/car/hyundai/hyundaicanfd.py
+++ opendbc_repo/opendbc/car/hyundai/hyundaicanfd.py
@@ -499,10 +499,7 @@ def create_ccnc_messages(CP, packer, CAN, frame, CC, CS, hud_control, disp_angle
           #values["FF_DETECT"] = 5 if hud_control.leadRelSpeed > -0.1 else 6 # truck
           values["FF_DETECT"] = 3 if hud_control.leadRelSpeed > -0.1 else 4 # car
 
-        values["FF_DETECT_POS"] = 10
-        values["FF_DETECT"] = 4
-        values["FF_DETECT_LAT"] = (frame / 10) % 128
-        print(f"LAT={values["FF_DETECT_LAT"]}")
+          values["FF_DETECT_POS"] = canfd_debug
 
 
         """
