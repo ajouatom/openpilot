2b59b21c1043352f1da3d268332eaa1bcd8c1239
ajouatom <ajouatom@gmail.com>
2025-05-01 15:55:26 +0900

    fix..

--- selfdrive/controls/lib/lateral_planner.py
+++ selfdrive/controls/lib/lateral_planner.py
@@ -139,8 +139,8 @@ class LateralPlanner:
     self.LP.curvature = measured_curvature
     self.path_xyz, self.lanelines_active = self.LP.get_d_path(sm['carState'], self.v_ego, self.t_idxs, self.path_xyz, self.curve_speed)
     
-    if self.LP.lanefull_mode:
-      self.plan_yaw, self.plan_yaw_rate = self.LP.calculate_plan_yaw_and_yaw_rate(self.path_xyz)
+    #if self.LP.lanefull_mode:
+    #  self.plan_yaw, self.plan_yaw_rate = self.LP.calculate_plan_yaw_and_yaw_rate(self.path_xyz)
       
     self.latDebugText = self.LP.debugText
     #self.lanelines_active = True if self.LP.d_prob > 0.3 and self.LP.lanefull_mode else False
