496dd0f33d8894183ea0f090ea60208876d02671
ajouatom <ajouatom@gmail.com>
2025-05-03 15:53:11 +0900

    comfortBrake 2.5 -> 2.4

--- selfdrive/carrot/carrot_functions.py
+++ selfdrive/carrot/carrot_functions.py
@@ -80,7 +80,7 @@ class CarrotPlanner:
 
     self.stop_distance = 6.0
     self.trafficStopDistanceAdjust = 1.0 #params.get_float("TrafficStopDistanceAdjust") / 100.
-    self.comfortBrake = 2.5
+    self.comfortBrake = 2.4
     self.comfort_brake = self.comfortBrake
 
     self.soft_hold_active = 0
