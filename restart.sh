c3a96ece01fa35ecd55bafb9f5911f2259b24548
ajouatom <ajouatom@gmail.com>
2025-05-03 16:42:36 +0900

    ff

--- restart.sh
+++ restart.sh
@@ -1,5 +1,2 @@
 git pull
-tmux kill-session -t comma 2>/dev/null
-rm -f /tmp/safe_staging_overlay.lock
-tmux new -s comma -d "cd /data/openpilot && ./launch_openpilot.sh"
-
+tmux kill-session -t comma; rm -f /tmp/safe_staging_overlay.lock; tmux new -s comma -d "/data/openpilot/launch_openpilot.sh"
