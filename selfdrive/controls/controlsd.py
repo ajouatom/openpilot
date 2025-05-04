0c6a64351435f5ec259591e280da052b6959b344
ajouatom <ajouatom@gmail.com>
2025-05-04 15:36:10 +0900

    fix..

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -25,7 +25,6 @@ from openpilot.selfdrive.controls.lib.longcontrol import LongControl
 from openpilot.common.realtime import DT_CTRL, DT_MDL
 from openpilot.selfdrive.modeld.constants import ModelConstants
 from openpilot.selfdrive.controls.lib.drive_helpers import CONTROL_N
-from selfdrive.modeld.modeld import LAT_SMOOTH_SECONDS
 
 State = log.SelfdriveState.OpenpilotState
 LaneChangeState = log.LaneChangeState
