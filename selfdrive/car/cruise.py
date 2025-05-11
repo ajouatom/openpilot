621b27b1e39b9d4faa1667fc041bbe28fa2ab02a
ajouatom <ajouatom@gmail.com>
2025-05-11 15:08:07 +0900

    fix..

--- selfdrive/car/cruise.py
+++ selfdrive/car/cruise.py
@@ -482,10 +482,10 @@ class VCruiseCarrot:
           self._cruise_control(-1, -1, "Cruise off,softhold mode (decelCruise)")
         elif self._cruise_ready:
           pass
-        elif self.v_ego_kph_set > v_cruise_kph + 2:
+        elif self.v_ego_kph_set > v_cruise_kph + 2 and self._cruise_button_mode in [2]:
           v_cruise_kph = max(self.v_ego_kph_set, self._cruise_speed_min)
-        #elif self._cruise_button_mode in [0, 1]:
-        #  v_cruise_kph = button_kph
+        elif self._cruise_button_mode in [0, 1]:
+          v_cruise_kph = button_kph
         elif self.v_ego_kph_set > self._cruise_speed_min and v_cruise_kph > self.v_ego_kph_set:
           v_cruise_kph = self.v_ego_kph_set
         else:
