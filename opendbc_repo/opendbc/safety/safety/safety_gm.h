762d28d0fc3fb0a73668381679c3bb89c01721dd
SangJo Kim <kandrea@nate.com>
2025-05-16 13:56:46 +0900

    check brake signal & pressure

--- opendbc_repo/opendbc/safety/safety/safety_gm.h
+++ opendbc_repo/opendbc/safety/safety/safety_gm.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 #include "safety_declarations.h"
 
@@ -74,12 +74,20 @@ static void gm_rx_hook(const CANPacket_t *to_push) {
 
     // Reference for brake pressed signals:
     // https://github.com/commaai/openpilot/blob/master/selfdrive/car/gm/carstate.py
-    if ((addr == 0xBE) && (gm_hw == GM_ASCM)) {
-      brake_pressed = GET_BYTE(to_push, 1) >= 10U;
+    if (gm_hw == GM_ASCM) {
+      if (addr == 0xBE) {
+        brake_pressed = GET_BYTE(to_push, 1) >= 10U; //핑거190 브레이크답력
+      }
+      if (addr == 0xF1) {
+        brake_pressed = GET_BYTE(to_push, 1) >= 15U; //핑거241 브레이크답력
+      }
     }
 
-    if ((addr == 0xC9) && (gm_hw == GM_CAM)) {
-      brake_pressed = GET_BIT(to_push, 40U);
+    if (addr == 0xC9) {
+      if (gm_hw == GM_CAM) {
+        brake_pressed = GET_BIT(to_push, 40U);  // CAM_ACC차량용 브레이크 체크(201핑거 40번째 비트)
+      }
+      acc_main_on = GET_BIT(to_push, 29U);  // (오토)크루즈 메인스위치 체크(201핑거 29번째 비트)
     }
 
     if (addr == 0x1C4) {
