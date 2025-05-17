dbdb2a337d107a9979079af374de09a2eb3431bd
SangJo Kim <kandrea@nate.com>
2025-05-17 16:39:32 +0900

    fix event(accFault) not PERMANENT

--- selfdrive/selfdrived/events.py
+++ selfdrive/selfdrived/events.py
@@ -882,7 +882,7 @@ EVENTS: dict[int, dict[str, Alert | AlertCallbackType]] = {
 
   EventName.accFaulted: {
     ET.IMMEDIATE_DISABLE: ImmediateDisableAlert("Cruise accFault: Restart the Car"),
-    ET.PERMANENT: NormalPermanentAlert("Cruise Fault: Restart the car to engage"),
+    #ET.PERMANENT: NormalPermanentAlert("Cruise Fault: Restart the car to engage"),
     ET.NO_ENTRY: NoEntryAlert("Cruise Fault: Restart the Car"),
   },
 
