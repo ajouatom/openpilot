fc32ac82f4f4a170efe159ac916023f4c40a755c
SangJo Kim <kandrea@nate.com>
2025-05-17 13:37:32 +0900

    fix

--- opendbc_repo/opendbc/car/gm/interface.py
+++ opendbc_repo/opendbc/car/gm/interface.py
@@ -220,13 +220,12 @@ class CarInterface(CarInterfaceBase):
 
     elif candidate in CAR.CHEVROLET_MALIBU_2019:
       ret.steerActuatorDelay = 0.2
-      ret.steerActuatorDelay = 0.45 if useEVTables else 0.3
       ret.longitudinalTuning.kpBP = [0.]
       ret.longitudinalTuning.kpV = [1.0]
       ret.longitudinalTuning.kiBP = [0.]
-      ret.longitudinalTuning.kiV = [.35]
+      ret.longitudinalTuning.kiV = [.1]
       ret.longitudinalTuning.kf = 1.0
-      ret.stoppingDecelRate = 0.2 # brake_travel/s while trying to stop
+      ret.stoppingDecelRate = 1.2 # brake_travel/s while trying to stop
       ret.vEgoStopping = 0.2
       ret.vEgoStarting = 0.15
       ret.stopAccel = -0.4
