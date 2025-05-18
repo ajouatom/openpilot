be84f517f18a7d309197241606b96124cd8123da
ajouatom <ajouatom@gmail.com>
2025-05-18 17:52:11 +0900

    fix updated...

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -120,7 +120,7 @@ procs = [
   PythonProcess("radard", "selfdrive.controls.radard", only_onroad),
   PythonProcess("hardwared", "system.hardware.hardwared", always_run),
   PythonProcess("tombstoned", "system.tombstoned", always_run, enabled=not PC),
-  PythonProcess("updated", "system.updated.updated", only_offroad, enabled=not PC),
+  PythonProcess("updated", "system.updated.updated", enable_updated, enabled=not PC),
   #PythonProcess("uploader", "system.loggerd.uploader", always_run),
   PythonProcess("statsd", "system.statsd", always_run),
 
