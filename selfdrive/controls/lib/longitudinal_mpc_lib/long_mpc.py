c65045346d042cb52d62de3c3aa56a878799dd5e
ajouatom <ajouatom@gmail.com>
2025-05-18 18:19:06 +0900

    ff

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -10,7 +10,7 @@ from openpilot.common.swaglog import cloudlog
 from openpilot.selfdrive.modeld.constants import index_function
 from openpilot.selfdrive.controls.radard import _LEAD_ACCEL_TAU
 # from openpilot.selfdrive.carrot.carrot_functions import CarrotPlanner
-from openpilot.selfdrive.carrot.carrot_function import XState
+from openpilot.selfdrive.carrot.carrot_functions import XState
 
 if __name__ == '__main__':  # generating code
   from openpilot.third_party.acados.acados_template import AcadosModel, AcadosOcp, AcadosOcpSolver
