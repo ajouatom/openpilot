4ad91724b9531603dbe4141971ce7d6e3f94c296
SangJo Kim <kandrea@nate.com>
2025-05-19 14:15:53 +0900

    fix Malibu_2019

--- opendbc_repo/opendbc/car/gm/interface.py
+++ opendbc_repo/opendbc/car/gm/interface.py
@@ -223,9 +223,9 @@ class CarInterface(CarInterfaceBase):
       ret.longitudinalTuning.kpBP = [0.]
       ret.longitudinalTuning.kpV = [1.0]
       ret.longitudinalTuning.kiBP = [0.]
-      ret.longitudinalTuning.kiV = [.1]
+      ret.longitudinalTuning.kiV = [0.]
       ret.longitudinalTuning.kf = 1.0
-      ret.stoppingDecelRate = 1.2 # brake_travel/s while trying to stop
+      ret.stoppingDecelRate = 0.2 # brake_travel/s while trying to stop
       ret.vEgoStopping = 0.2
       ret.vEgoStarting = 0.15
       ret.stopAccel = -0.4
