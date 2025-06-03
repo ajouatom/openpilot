0722bc8e902f7437841e6c5bf17c4676f9112dc5
ajouatom <ajouatom@gmail.com>
2025-06-03 13:39:01 +0900

    fix.. typo

--- common/params_keys.h
+++ common/params_keys.h
@@ -237,7 +237,7 @@ inline static std::unordered_map<std::string, uint32_t> keys = {
     {"UseLaneLineSpeedApply", PERSISTENT},
     {"AdjustLaneOffset", PERSISTENT},
     {"LaneChangeNeedTorque", PERSISTENT},
-    {"LaneChangeDelay", PERSISENT},
+    {"LaneChangeDelay", PERSISTENT },
     {"LaneChangeBsd", PERSISTENT},
     {"MaxAngleFrames", PERSISTENT},
     {"SoftHoldMode", PERSISTENT},
