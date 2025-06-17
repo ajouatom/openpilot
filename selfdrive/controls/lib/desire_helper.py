3a5fde85a673df08565b81b8c9b7bf8a97771ddc
carrot <43668841+ajouatom@users.noreply.github.com>
2025-06-17 22:08:31 +0900

    Update desire_helper.py

--- selfdrive/controls/lib/desire_helper.py
+++ selfdrive/controls/lib/desire_helper.py
@@ -279,7 +279,7 @@ class DesireHelper:
     lane_width_diff = self.lane_width_left_diff if atc_blinker_state == BLINKER_LEFT else self.lane_width_right_diff
     distance_to_road_edge = self.distance_to_road_edge_left if atc_blinker_state == BLINKER_LEFT else self.distance_to_road_edge_right
     lane_width_side = self.lane_width_left if atc_blinker_state == BLINKER_LEFT else self.lane_width_right
-    if lane_width_diff > 0.5 and (lane_width_side > distance_to_road_edge):
+    if lane_width_diff > 0.5 and (lane_width_side < distance_to_road_edge):
       lane_availabled = True
     edge_availabled = not self.edge_available_last and edge_available
     side_object_detected = self.object_detected_count > -0.3 / DT_MDL
