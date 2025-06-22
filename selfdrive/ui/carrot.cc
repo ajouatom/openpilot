961e35e9b7e7d05d278b71491e65ca0df391d1a3
ajouatom <ajouatom@gmail.com>
2025-06-23 08:26:29 +0900

    fix..

--- selfdrive/ui/carrot.cc
+++ selfdrive/ui/carrot.cc
@@ -2788,7 +2788,7 @@ public:
         const auto live_delay = sm["liveDelay"].getLiveDelay();
         const auto live_torque_params = sm["liveTorqueParameters"].getLiveTorqueParameters();
         const auto live_params = sm["liveParameters"].getLiveParameters();
-        str.sprintf("LD[%.1f%%,%.2f],LT[%.1f%%,%s](%.2f/%.2f), SR(%.1f,%.1f)",
+        str.sprintf("LD[%.0f%%,%.2f],LT[%.0f%%,%s](%.2f/%.2f), SR(%.1f,%.1f)",
             live_delay.getCalPerc(), live_delay.getLateralDelay(),
             live_torque_params.getCalPerc(), live_torque_params.getLiveValid() ? "ON" : "OFF",
             live_torque_params.getLatAccelFactorFiltered(), live_torque_params.getFrictionCoefficientFiltered(),
