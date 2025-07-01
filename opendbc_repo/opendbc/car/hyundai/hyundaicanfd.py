ea7e49cb177f31b59ca059f350143142b8b51d9f
ajouatom <ajouatom@gmail.com>
2025-07-01 19:17:59 +0900

    fix..

--- opendbc_repo/opendbc/car/hyundai/hyundaicanfd.py
+++ opendbc_repo/opendbc/car/hyundai/hyundaicanfd.py
@@ -277,7 +277,7 @@ def create_acc_control_scc2(packer, CAN, enabled, accel_last, accel, stopping, g
   acc_mode = 0 if not enabled else (2 if gas_override else 1)
   if CS.out.carrotCruise > 0:
     if CS.softHoldActive == 0:
-      if accel > -0.2 and not stopping:
+      if accel > -0.1 and not stopping:
         acc_mode = 2 if enabled else 0
         enabled = False
         
