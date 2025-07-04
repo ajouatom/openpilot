c644b0c603c72bbe4b210dc325238fd8bd2b8bff
ajouatom <ajouatom@gmail.com>
2025-07-04 21:51:01 +0900

    fix.. CC

--- selfdrive/car/cruise.py
+++ selfdrive/car/cruise.py
@@ -517,6 +517,8 @@ class VCruiseCarrot:
           v_cruise_kph = button_kph
         elif self.v_ego_kph_set > self._cruise_speed_min and v_cruise_kph > self.v_ego_kph_set:
           v_cruise_kph = self.v_ego_kph_set
+        elif self.v_ego_kph_set < 1.0:
+          self.carrot_cruise_active = True
         else:
           #self._cruise_control(-2, -1, "Cruise off (decelCruise)")
           self.carrot_cruise_active = True
