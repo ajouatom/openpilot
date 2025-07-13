233ba9ce1eb110c158241700c3b67fe5d4591dad
ajouatom <ajouatom@gmail.com>
2025-07-13 21:24:46 +0900

    fix.. experimental.. long

--- selfdrive/controls/lib/drive_helpers.py
+++ selfdrive/controls/lib/drive_helpers.py
@@ -115,14 +115,16 @@ def get_accel_from_plan(speeds, accels, t_idxs, action_t=DT_MDL, vEgoStopping=0.
     v_target = np.interp(action_t, t_idxs, speeds)
     a_target = 2 * (v_target - v_now) / (action_t) - a_now
     v_target_1sec = np.interp(action_t + 1.0, t_idxs, speeds)
+    v_max = np.max(speeds)
   else:
     v_now = 0.0
     v_target = 0.0
     v_target_1sec = 0.0
     a_target = 0.0
+    v_max = 0.0
   should_stop = (v_target < vEgoStopping and
                  v_target_1sec < vEgoStopping)
-  return a_target, should_stop, v_target #v_now
+  return a_target, should_stop, v_max #v_target #v_now
 
 def curv_from_psis(psi_target, psi_rate, vego, action_t):
   vego = np.clip(vego, MIN_SPEED, np.inf)
