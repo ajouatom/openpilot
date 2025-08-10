8b77ee9decc299f21e3c8408e6e30e1aabf24c3c
ajouatom <ajouatom@gmail.com>
2025-08-10 15:54:02 +0900

    fix..

--- selfdrive/car/cruise.py
+++ selfdrive/car/cruise.py
@@ -575,6 +575,7 @@ class VCruiseCarrot:
         self._lat_enabled = False
         self._paddle_decel_active = False
         self.params.put_bool_nonblocking("ExperimentalMode", not self.params.get_bool("ExperimentalMode"))
+        self._add_log("Lateral " + "enabled" if self._lat_enabled else "disabled")
 
     if self._paddle_mode > 0 and button_type in [ButtonType.paddleLeft, ButtonType.paddleRight]:  # paddle button
       self._cruise_control(-2, -1, "Cruise off & Ready (paddle)")
