ede5f7052ac4abcd97859b1f703e1728216f2e9b
ajouatom <ajouatom@gmail.com>
2025-08-10 14:42:22 +0900

    fix..

--- selfdrive/controls/radard.py
+++ selfdrive/controls/radard.py
@@ -132,16 +132,16 @@ def match_vision_to_track(v_ego: float, lead: capnp._DynamicStructReader, tracks
       best_score = score
       best_track = c
 
+  if best_track is not None and offset_vision_dist - best_track.dRel > max_offset_vision_dist: 
+    best_track = None
+
+  #if lead.v[0] - best_track.vLead > max_offset_vision_vel:
+  #  best_track = None
+
+  if best_track is not None and abs(best_track.yRel + best_track.yvLead * radar_lat_factor + lead.y[0]) > 3.0: # lead.y[0]는 반대..
+    best_track = None
 
   if best_track is not None:
-    if offset_vision_dist - best_track.dRel > max_offset_vision_dist: 
-      best_track = None
-
-    #if lead.v[0] - best_track.vLead > max_offset_vision_vel:
-    #  best_track = None
-
-    if abs(best_track.yRel + best_track.yvLead * radar_lat_factor + lead.y[0]) > 3.0: # lead.y[0]는 반대..
-      best_track = None
 
     if lead.v[0] - best_track.vLead > max_offset_vision_vel:
       best_track.is_stopped_car_count += 1
