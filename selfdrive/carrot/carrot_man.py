b548ea567702358e235b3586ebe48673c3002475
ajouatom <ajouatom@gmail.com>
2025-08-14 11:43:51 +0900

    fix..

--- selfdrive/carrot/carrot_man.py
+++ selfdrive/carrot/carrot_man.py
@@ -1516,7 +1516,7 @@ class CarrotServ:
 
       self.nGoPosDist = int(msg_nav.distanceRemaining)
       self.nGoPosTime = int(msg_nav.timeRemaining)
-      if self.active_kisa_count <= 0:
+      if self.active_kisa_count <= 0 and msg_nav.speedLimit > 0:
         self.nRoadLimitSpeed = max(30, round(msg_nav.speedLimit * CV.MS_TO_KPH))
       self.xDistToTurn = int(msg_nav.maneuverDistance)
       self.szTBTMainText = msg_nav.maneuverPrimaryText
