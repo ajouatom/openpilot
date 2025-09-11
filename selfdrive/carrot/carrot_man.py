bcd878f280e6cba03cd5c2a2c888dd3e55479d71
ajouatom <ajouatom@gmail.com>
2025-09-11 17:50:45 +0900

    fix.

--- selfdrive/carrot/carrot_man.py
+++ selfdrive/carrot/carrot_man.py
@@ -630,6 +630,12 @@ class CarrotMan:
       car_selected = car_selected.decode('utf-8')
 
     git_branch = Params().get("GitBranch").decode('utf-8')
+    try:
+      ftp.mkd(git_branch)
+    except Exception as e:
+      print(f"Directory creation failed: {e}")
+    ftp.cwd(directory)
+    
     directory = git_branch + "/" + car_selected + " " + Params().get("DongleId").decode('utf-8')
     current_time = datetime.now().strftime("%Y%m%d-%H%M%S")
     filename = tmux_why + "-" + current_time + "-" + git_branch + ".txt"
