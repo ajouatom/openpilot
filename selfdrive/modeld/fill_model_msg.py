ab7596f6631a4c72b5eb04c93f8ec266ad2da8fd
ajouatom <ajouatom@gmail.com>
2025-11-10 18:14:50 +0900

    fix..

--- selfdrive/modeld/fill_model_msg.py
+++ selfdrive/modeld/fill_model_msg.py
@@ -102,21 +102,35 @@ def fill_model_msg(base_msg: capnp._DynamicStructBuilder, extended_msg: capnp._D
   LINE_T_IDXS = [np.nan] * ModelConstants.IDX_N
   LINE_T_IDXS[0] = 0.0
   plan_x = net_output_data['plan'][0, :, Plan.POSITION][:, 0].tolist()
+  Tmax = ModelConstants.T_IDXS[ModelConstants.IDX_N - 1]
   for xidx in range(1, ModelConstants.IDX_N):
     tidx = 0
     # increment tidx until we find an element that's further away than the current xidx
     while tidx < ModelConstants.IDX_N - 1 and plan_x[tidx + 1] < ModelConstants.X_IDXS[xidx]:
       tidx += 1
     if tidx == ModelConstants.IDX_N - 1:
-      # if the Plan doesn't extend far enough, set plan_t to the max value (10s), then break
-      LINE_T_IDXS[xidx] = ModelConstants.T_IDXS[ModelConstants.IDX_N - 1]
-      break
+      for k in range(xidx, ModelConstants.IDX_N):
+        LINE_T_IDXS[k] = Tmax
+      break  
     # interpolate to find `t` for the current xidx
     current_x_val = plan_x[tidx]
     next_x_val = plan_x[tidx + 1]
-    p = (ModelConstants.X_IDXS[xidx] - current_x_val) / (next_x_val - current_x_val) if abs(
-      next_x_val - current_x_val) > 1e-9 else float('nan')
-    LINE_T_IDXS[xidx] = p * ModelConstants.T_IDXS[tidx + 1] + (1 - p) * ModelConstants.T_IDXS[tidx]
+
+    dx = next_x_val - current_x_val
+    if dx <= 1e-9:
+      LINE_T_IDXS[xidx] = ModelConstants.T_IDXS[tidx]
+    else:
+      p = (ModelConstants.X_IDXS[xidx] - current_x_val) / dx
+      if p < 0.0: p = 0.0                                   
+      elif p > 1.0: p = 1.0                                 
+      LINE_T_IDXS[xidx] = p * ModelConstants.T_IDXS[tidx + 1] + (1.0 - p) * ModelConstants.T_IDXS[tidx]
+
+    #p = (ModelConstants.X_IDXS[xidx] - current_x_val) / (next_x_val - current_x_val) if abs(
+    #  next_x_val - current_x_val) > 1e-9 else float('nan')
+    #LINE_T_IDXS[xidx] = p * ModelConstants.T_IDXS[tidx + 1] + (1 - p) * ModelConstants.T_IDXS[tidx]
+
+  LINE_T_IDXS = [Tmax if not (v == v) else v for v in LINE_T_IDXS]     # // 추가 (NaN → Tmax)
+  LINE_T_IDXS = list(np.maximum.accumulate(LINE_T_IDXS))               # // 추가 (단조성 보장)
 
   # lane lines
   modelV2.init('laneLines', 4)
