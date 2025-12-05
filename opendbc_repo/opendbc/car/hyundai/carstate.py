1919821a07e9846a237d63930a85eb7a4668af9f
ajouatom <ajouatom@gmail.com>
2025-12-05 17:29:33 +0900

    fix steering_angle(anglecontrol car)

--- opendbc_repo/opendbc/car/hyundai/carstate.py
+++ opendbc_repo/opendbc/car/hyundai/carstate.py
@@ -456,7 +456,7 @@ class CarState(CarStateBase):
     # steering angle deg값이 이상함. mdps값이 더 신뢰가 가는듯.. torque steering 차량도 확인해야함.
     #ret.steeringAngleDeg = cp.vl["STEERING_SENSORS"]["STEERING_ANGLE"] * -1
     if self.CP.flags & HyundaiFlags.ANGLE_CONTROL:
-      ret.steeringAngleDeg = cp.vl["MDPS"]["STEERING_ANGLE"] * -1
+      ret.steeringAngleDeg = cp.vl["MDPS"]["STEERING_ANGLE_2"] * -1
     else:
       ret.steeringAngleDeg = cp.vl["STEERING_SENSORS"]["STEERING_ANGLE"] * -1
     
