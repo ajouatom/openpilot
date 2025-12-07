111d49301fb09ad48db3eb075e6c1e4c2e61bf7d
ajouatom <ajouatom@gmail.com>
2025-12-07 17:07:15 +0900

    model turn.

--- selfdrive/carrot/carrot_serv.py
+++ selfdrive/carrot/carrot_serv.py
@@ -982,9 +982,13 @@ class CarrotServ:
     if self.turnSpeedControlMode == 2:
       if -500 < self.xDistToTurn < 500:
         speed_n_sources.append((route_speed, "route"))
-    elif self.turnSpeedControlMode == 3:
+    elif self.turnSpeedControlMode in [3, 4]:
       speed_n_sources.append((route_speed, "route"))
       #speed_n_sources.append((self.calculate_current_speed(dist, speed * self.mapTurnSpeedFactor, 0, 1.2), "route"))
+
+    model_turn_speed = max(sm['modelV2'].meta.modelTurnSpeed, self.autoCurveSpeedLowerLimit)
+    if model_turn_speed < 200 and abs(vturn_speed) < 120:
+      speed_n_sources.append((model_turn_speed, "model"))
 
     desired_speed, source = min(speed_n_sources, key=lambda x: x[0])
 
