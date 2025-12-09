5227329b0021ff7ef90f21e7e26e427600994c10
ajouatom <ajouatom@gmail.com>
2025-12-09 17:56:16 +0900

    fix ..

--- selfdrive/carrot/carrot_man.py
+++ selfdrive/carrot/carrot_man.py
@@ -367,7 +367,7 @@ class CarrotMan:
             self.v_cruise_change = 0
         elif self.v_cruise_last > CS.vCruise: # 속도가 감소하면
           if v_ego_kph < CS.vCruise: # 주행속도가 느리면
-            self.v_cruise_change = 0 #100
+            self.v_cruise_change = -100 #100
           else:                       # 주행속도가 빠르면
             self.v_cruise_change = -100
 
