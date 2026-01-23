b88f5f79661ce9bdcb4bad4666de414380f72a63
ajouatom <ajouatom@gmail.com>
2026-01-23 19:51:33 +0900

    fix..

--- selfdrive/selfdrived/alertmanager.py
+++ selfdrive/selfdrived/alertmanager.py
@@ -13,11 +13,11 @@ with open(os.path.join(BASEDIR, "selfdrive/selfdrived/alerts_offroad.json")) as 
   OFFROAD_ALERTS = json.load(f)
 
 
-def set_offroad_alert(alert: str, show_alert: bool, extra_text: str = None) -> None:
+def set_offroad_alert(alert: str, show_alert: bool, extra_text: str | None = None) -> None:
   if show_alert:
     a = copy.copy(OFFROAD_ALERTS[alert])
     a['extra'] = extra_text or ''
-    Params().put(alert, json.dumps(a))
+    Params().put(alert, a)
   else:
     Params().remove(alert)
 
