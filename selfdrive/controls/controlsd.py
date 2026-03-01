59e9dafe71f7f08be212b785f06a1e0994cf2cbb
fullmetalsonic <update0401@gmail.com>
2026-03-01 21:33:18 +0900

    Update controlsd.py

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -130,8 +130,8 @@ class Controls:
 
     # Enable blinkers while lane changing
     if model_v2.meta.laneChangeState != LaneChangeState.off:
-      CC.leftBlinker = (model_v2.meta.laneChangeDirection == LaneChangeDirection.left) and CS.leftBlinker
-      CC.rightBlinker = (model_v2.meta.laneChangeDirection == LaneChangeDirection.right) and CS.rightBlinker
+      CC.leftBlinker = model_v2.meta.laneChangeDirection == LaneChangeDirection.left
+      CC.rightBlinker = model_v2.meta.laneChangeDirection == LaneChangeDirection.right
 
     if not CC.latActive:
       self.LaC.reset()
