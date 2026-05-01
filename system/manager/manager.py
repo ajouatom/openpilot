13c5e8d4ff4dfd2c81664a9c251fd83096fb7867
ajouatom <ajouatom@gmail.com>
2026-05-01 19:55:25 +0900

    remove memory print
    (cherry picked from commit 09a7b5d116ee4f791715722f2983d9b64c2a6d60)

--- system/manager/manager.py
+++ system/manager/manager.py
@@ -151,7 +151,6 @@ def manager_thread() -> None:
   started_prev = False
   ignition_prev = False
 
-  mem_print_t = 0.0
   while True:
     sm.update(1000)
     now = time.monotonic()
@@ -175,16 +174,6 @@ def manager_thread() -> None:
     ignition_prev = ignition
 
     ensure_running(managed_processes.values(), started, params=params, CP=sm['carParams'], not_run=ignore)
-
-    if now - mem_print_t > 10.0:
-      mem_print_t = now
-      mem_lines = []
-      for p in managed_processes.values():
-        if p.proc and p.proc.is_alive():
-          pid = p.proc.pid
-          rss_kb = read_rss_kb(pid)
-          mem_lines.append(f"{p.name}[{pid}]={rss_kb/1024:.1f}MB")
-      print("PROC_MEM " + " | ".join(mem_lines))    
 
     running = ' '.join("{}{}\u001b[0m".format("\u001b[32m" if p.proc.is_alive() else "\u001b[31m", p.name)
                        for p in managed_processes.values() if p.proc)
