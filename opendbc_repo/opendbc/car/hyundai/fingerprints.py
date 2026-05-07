80d8c241e8921510a6b8ef6f996a4c30e3595394
qqunilove <66712662+qqunilove@users.noreply.github.com>
2026-05-07 15:12:25 +0900

    Update fingerprints.py

--- opendbc_repo/opendbc/car/hyundai/fingerprints.py
+++ opendbc_repo/opendbc/car/hyundai/fingerprints.py
@@ -527,6 +527,14 @@ FW_VERSIONS = {
       b'\xf1\x00IK  MFC  AT USA LHD 1.00 1.04 99211-G9000 220401',
     ],
   },
+  CAR.GENESIS_G70_2024: {
+    (Ecu.fwdCamera, 0x7c4, None): [
+      b'\xf1\x00IK  MFC  AT USA LHD 1.00 1.02 99211-G9500 230712',
+    ],
+    (Ecu.fwdRadar, 0x7d0, None): [
+      b'\xf1\x00IKPE RDR -----      1.00 1.00 99110-G9600         ',
+    ],
+  },  
   CAR.GENESIS_G80: {
     (Ecu.fwdRadar, 0x7d0, None): [
       b'\xf1\x00DH__ SCC F-CU-      1.00 1.01 96400-B1110         ',
