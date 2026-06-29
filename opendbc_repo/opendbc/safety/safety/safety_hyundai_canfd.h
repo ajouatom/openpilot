d2982f2292931728132179ffa45b138c952f879d
ajouatom <ajouatom@gmail.com>
2026-06-29 22:17:52 +0900

    Avoid libc memcpy in Hyundai safety
    (cherry picked from commit 7992dfc2457d792fdc42ca6e1ce6b14bdba35a06)

--- opendbc_repo/opendbc/safety/safety/safety_hyundai_canfd.h
+++ opendbc_repo/opendbc/safety/safety/safety_hyundai_canfd.h
@@ -370,7 +370,9 @@ static void canfd_copy_packet(CANPacket_t* dst, const CANPacket_t* src) {
   dst->addr = src->addr;
   dst->bus = src->bus;
   dst->data_len_code = src->data_len_code;
-  (void)memcpy(dst->data, src->data, dlc_to_len[src->data_len_code]);
+  for (uint8_t i = 0U; i < GET_LEN(src); i++) {
+    dst->data[i] = src->data[i];
+  }
 }
 static CanfdBufferedFwd* canfd_bfwd_find(int addr, int dst_bus) {
   for (int i = 0; canfd_bfwd[i].addr > 0; i++) {
