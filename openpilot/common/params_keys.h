0cdd0fd6b25358084e230ce95164c700a8b395df
ajouatom <ajouatom@gmail.com>
2026-08-30 21:14:39 +0900

    longitudinal: consolidate lead response tuning
    (cherry picked from commit 3a81a3b4694dc6391361aa66e7fb9e4371099906)

--- openpilot/common/params_keys.h
+++ openpilot/common/params_keys.h
@@ -255,7 +255,6 @@ inline static std::unordered_map<std::string, ParamKeyAttributes> keys = {
     {"AutoRoadSpeedAdjust", {PERSISTENT, INT, "50"}},
 
     {"StopDistanceCarrot", {PERSISTENT, INT, "550"}},
-    {"JLeadFactor3", {PERSISTENT, INT, "0"}},
     {"LeadResponseMode", {PERSISTENT, INT, "0"}},
     {"CruiseButtonMode", {PERSISTENT, INT, "0"}},
     {"CancelButtonMode", {PERSISTENT, INT, "0"}},
@@ -297,7 +296,6 @@ inline static std::unordered_map<std::string, ParamKeyAttributes> keys = {
     {"LongActuatorDelay", {PERSISTENT, INT, "20"}},
     {"VEgoStopping", {PERSISTENT, INT, "50"}},
 
-    {"RadarReactionFactor", {PERSISTENT, INT, "100"}},
     {"EnableRadarTracks", {PERSISTENT, INT, "0"}},
     {"RadarLatFactor", {PERSISTENT, INT, "0"}},
     {"EnableCornerRadar", {PERSISTENT, INT, "0"}},
