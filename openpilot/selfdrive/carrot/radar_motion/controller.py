95816c0d01a3a05a1a98a1989374c7f831a01643
ajouatom <ajouatom@gmail.com>
2026-09-02 16:19:36 +0900

    carrot: add guarded corner vision leadOne fallback
    (cherry picked from commit 278171a9bc393bbad1d4e4e243c47a9b69387608)

--- openpilot/selfdrive/carrot/radar_motion/controller.py
+++ openpilot/selfdrive/carrot/radar_motion/controller.py
@@ -23,6 +23,7 @@ from openpilot.selfdrive.carrot.radar_motion.predictor import (
   project_to_model_path,
 )
 from openpilot.selfdrive.carrot.radar_motion.primary import (
+  CornerVisionPrimaryFallbackMatcher,
   FrontRadarKinematicAssociator,
   RadarPointSnapshot,
   VisionRadarMatcher,
@@ -359,6 +360,9 @@ class DPathRadarController:
   ) -> None:
     self.primary_matcher = VisionRadarMatcher()
     self.scc_primary_fallback_matcher = VisionRadarMatcher()
+    self.corner_vision_primary_fallback_matcher = (
+      CornerVisionPrimaryFallbackMatcher()
+    )
     self.enable_radar_tracks = int(enable_radar_tracks)
     self.front_radar_measurement_delay_s = max(
       0.0, float(front_radar_measurement_delay_s),
@@ -555,6 +559,7 @@ class DPathRadarController:
     if len(path) < 2:
       self.primary_matcher.reset()
       self.scc_primary_fallback_matcher.reset()
+      self.corner_vision_primary_fallback_matcher.reset()
       self.lead_two_tracker.reset()
       self.stationary_shadow_tracker.reset()
       self.stationary_primary_handoff_tracker.reset()
@@ -608,6 +613,18 @@ class DPathRadarController:
       )
     else:
       self.scc_primary_fallback_matcher.reset()
+    if primary_match is not None:
+      self.corner_vision_primary_fallback_matcher.reset()
+    elif self.enable_radar_tracks > 0:
+      primary_match = self.corner_vision_primary_fallback_matcher.update(
+        time_s,
+        model,
+        points,
+        path,
+        yaw_rate_rad_s,
+      )
+    else:
+      self.corner_vision_primary_fallback_matcher.reset()
     lead_one = None
     if primary_match is not None:
       lead_one = self._lead_from_radar_point(
